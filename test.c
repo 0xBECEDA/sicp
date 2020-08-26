d2f5cb0983c7610aa4b14979f6ddb63d7ef19d0e
rigidus <avenger-f@yandex.ru>
2020-08-26 08:16:32 +0300

    fix

--- test.c
+++ test.c
@@ -1,258 +1,252 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-
+#include <string.h>
 
 /* сборка и запуск: */
 /* gcc test.c -o test */
 /* ./test */
 
 
-/* структура значения ячейки*/
-typedef struct val {
+/* возможные типы значений */
+enum list_of_types {
+               TYPE_INT,
+               TYPE_CHAR,
+               TYPE_NIL,
+               TYPE_CELL,
+};
 
-    void * val;
-    int typeNum;
-
-}val;
-
-/* структура ячейки */
+/* тип списка */
 typedef struct cell {
-
-    struct val * value;
-    struct cell * next;
-
-}cell;
-
-// globals
-int a = 4;
-char b[1] = "b";
-int c = 5;
-
-/* создает пустой список  */
-cell* makeEmptyList() {
-
-    cell *lst = (cell*)malloc(sizeof(cell));
-    lst->value = NULL;
-    lst->next = NULL;
-    return lst;
-}
-/* создает первую ячейку списка */
-cell* initList(val *value) {
-    cell *lst = (cell*)malloc(sizeof(cell));
-    lst->value = value;
-    lst->next = NULL;
-    return lst;
-}
+    struct val * car;
+    struct val * cdr;
+} cell;
 
 
-/* печатает пустой список */
-int printEmptyList () {
-    puts("()");
+/* тип значения */
+typedef struct val {
+    int type_num;
+    /* значение может быть любого перечисленного типа */
+    union uni_val {
+        int* int_val;
+        char* char_val;
+        cell* cell_val;
+    } uni_val;
+} val;
+
+
+/* конструктор значения-ссылки на копию числа */
+val* int_val_constructor ( int* val ) {
+    struct val * retval = malloc(sizeof(val));
+    int* pnt = malloc(sizeof(int));
+    retval->uni_val.int_val = val; /* copy val, may be not need (todo) */
+    retval->type_num = TYPE_INT;
+    return retval;
 }
 
-/* создает список */
-cell* makelist (int amountElts, ...) {
+/* конструктор значения-ссылки на копию буквы */
+val* char_val_constructor ( char* val ) {
+    struct val * retval = malloc(sizeof(val));
+    char* pnt = malloc(sizeof(char));
+    retval->uni_val.char_val = val; /* copy val, may be not need (todo) */
+    retval->type_num = TYPE_CHAR;
+    return retval;
+}
 
-    cell *lst, *curCell, *prevCell;
-    val * value = (val*)malloc(sizeof(val));
+/* конструктор NIL-значения */
+val* nil_constructor () {
+    struct val * retval = malloc(sizeof(cell));
+    retval->type_num = TYPE_NIL;
+    return retval;
+}
 
-    va_list values;
-    va_start(values, amountElts);
-    *value = va_arg(values, val);
-    /* проинициализировали список первым элементов */
-    lst = initList(value);
+/* конструктор значения-ссылки на ячейку */
+val* cell_val_constructor ( cell* val ) {
+    struct val * retval = malloc(sizeof(cell));
+    retval->uni_val.cell_val = val;
+    retval->type_num = TYPE_CELL;
+    return retval;
+}
 
-    amountElts--;
-    prevCell = lst;
+/* конструктор списка */
+val* cons  (val* car, val* cdr ) {
+    cell *cell = malloc(sizeof(cell));
+    cell->car = car;
+    cell->cdr = cdr;
+    return cell_val_constructor( cell );
+}
 
-    for (int i = 0; i <= amountElts; i++) {
-        /* элементы кончились */
-        if (i == amountElts) {
-            /* создаем путую ячейку и присоединяем ее к списку */
-            curCell = (cell*)malloc(sizeof(cell));
-            curCell = makeEmptyList();
-            prevCell->next = curCell;
-            va_end(values);
-        } else {
-            /* иначе создаем новую ячейку */
-            curCell = (cell*)malloc(sizeof(cell));
-            /* выделили память под структуру значения */
-            value = (val*)malloc(sizeof(val));
-            /* записали в нее структуру значения */
-            *value = va_arg(values, val);
-            /* записали структуру как значение ячейки */
-            curCell->value = value;
-            /* создали пустой указатель на след. ячейку */
-            curCell->next = NULL;
-            /* связали предыдущую и текущую ячейки указателем */
-            prevCell->next = curCell;
-            /* запомнили текущую структуру */
-            prevCell = curCell;
-        }
+
+int atom_predicate (int type_num) {
+    if ((TYPE_CHAR == type_num ) ||
+        (TYPE_INT == type_num )) {
+        return 1;
     }
-    return lst;
-}
-
-/* печатает значение всех ячеек списка */
-int printListValues (cell *lst) {
-
-    /* если список пустой или у текущей ячейки нет значения */
-    if (lst == NULL || lst->value == NULL) {
-        /* выход */
         return 0;
-
-        /* иначе */
-    } else {
-
-        /* получили структуру значения ячейки */
-        cell * curCell = lst;
-        val * curValue = curCell->value;
-
-        /* поулучили порядковый номер типа и завели пару указателей */
-        int typeNum = curValue->typeNum;
-        int *pnt;
-        char *pointer;
-
-        switch (typeNum) {
-
-        /* значение изначально было интом */
-        case 0:
-            /* печатаем как инт */
-            pnt = (int*)curValue->val;
-            printf("%d ", *pnt);
-            printListValues(curCell->next);
-            break;
-
-         /* значение изначально было char */
-        case 1:
-            pointer = (char*)curValue->val;
-            printf("%s ", pointer);
-            printListValues(curCell->next);
-            break;
-
-        default:
-            printf("printListValues: не распознан тип значения");
-        }
-    }
 }
-/* печатает список в лисповом виде */
-int printList (cell *lst) {
 
-    if (lst == NULL) {
-        printf("printList: передан указатль NULL\n");
-        /* return -1; */
 
-    /* это пустой список */
-    }else if (lst->value == NULL) {
+void pprint(val* param); /* forward declaration */
 
-        printEmptyList();
 
+void wrap_brackets_if_not_atom (val* car) {
+    if ( atom_predicate( car->type_num ) ) {
+        pprint( car );
     } else {
-        cell * curCell = lst;
         printf("(");
-        printListValues (curCell);
-        printf(")\n");
-        return 0;
+        pprint( car );
+        printf(")");
     }
 }
 
-/* тест печати */
-int testPrintList (cell *lst) {
-
-    printf("testPrintList: lst %p\n", lst);
-    printList(lst);
-    return 0;
+/* выводит список */
+void pprint(val* param) {
+    int tmp_int;
+    char tmp_char[1];
+    val* car;
+    val* cdr;
+    switch ( param->type_num ) {
+    case TYPE_INT:
+        tmp_int = *param->uni_val.int_val;
+        printf( "%d", tmp_int );
+        return;
+    case TYPE_CHAR:
+        strncpy( tmp_char, ((char*)(param->uni_val.char_val)), 1 );
+        printf( "%s", tmp_char );
+        return;
+    case TYPE_NIL:
+        printf( "NIL" );
+        return;
+    case TYPE_CELL:
+        car = param->uni_val.cell_val->car;
+        cdr = param->uni_val.cell_val->cdr;
+        if  (         ( TYPE_NIL == cdr->type_num ) ) {
+            /* это одноэлементный список (cdr = NIL) */
+            wrap_brackets_if_not_atom( car );
+        } else if (   ( TYPE_CHAR == cdr->type_num ) ||
+                      ( TYPE_INT  == cdr->type_num ) ) {
+            /* это точечная пара (cdr = ATOM)  */
+            pprint( car );
+            printf(" . ");
+            pprint( cdr );
+        } else if (   ( TYPE_CELL == cdr->type_num ) ) {
+            /* это список (cdr = CELL) */
+            if ( atom_predicate( car->type_num ) ) {
+                pprint( car );
+            } else {
+                printf("(");
+                pprint( car );
+                printf(")");
+            }
+            printf(" ");
+            pprint( cdr );
+        } else {
+            printf( "\nERR: UNIMPLEMENTED CELL %d : %d", car->type_num, cdr->type_num);
+        }
+        return;
+    default:
+        printf( "\nERR: UNIMPLEMENTED");
+    }
 }
 
-/* тест создания списка и его печати */
-/* возвращает указатель на список */
-cell* testMakeList() {
-
-    /* инициализируем структуры, которые будут значениями ячеек списка */
-    /* в мэйне есть точно такие же */
-
-    val *aStruct = (val*)malloc(sizeof(val));
-    val *bStruct = (val*)malloc(sizeof(val));
-    val *cStruct = (val*)malloc(sizeof(val));
-
-    aStruct->val = (void*)&a;
-    bStruct->val = (void*)&b;
-    cStruct->val = (void*)&c;
-
-    /* помечаем типы порядковым номером, */
-    /* чтоб можно было привести значения к изначальным типам */
-
-    /* типу инт соответствует 0 */
-
-    aStruct->typeNum = 0;
-    cStruct->typeNum = 0;
-    bStruct->typeNum = 1;
-
-    val struct1 = *aStruct;
-    val struct2 = *bStruct;
-    val struct3 = *cStruct;
-
-    /* создаем список */
-    cell * lst1 = makelist(3, struct1, struct2, struct3);
-
-    printf("printList from testMakeList:\n");
-
-    testPrintList(lst1);
-
-    testPrintList(lst1);
-
-    testPrintList(lst1);
-
-    return lst1;
+/* вызывает вывод списка, оборачивая скобками и todo:устанавливая отступы */
+void ipprint (val* param) {
+    printf( "(" );
+    pprint( param );
+    printf( ")" );
 }
+
 
 int main (void) {
 
     /* инициализируем структуры, которые будут значениями ячеек списка */
-    val *aStruct = (val*)malloc(sizeof(val));
-    val *bStruct = (val*)malloc(sizeof(val));
-    val *cStruct = (val*)malloc(sizeof(val));
+    int* ptr_a = malloc(sizeof(int));
+    *ptr_a = 4;
+    val* a_val = int_val_constructor( ptr_a );
 
-    aStruct->val = (void*)&a;
-    bStruct->val = (void*)&b;
-    cStruct->val = (void*)&c;
+    char* ptr_b = malloc(sizeof(char[1]));
+    strncpy( ptr_b, "b", 1 );
+    val* b_val = char_val_constructor( ptr_b );
 
-    /* помечаем типы порядковым номером, */
-    /* чтоб можно было привести значения к изначальным типам */
+    int* ptr_c = malloc(sizeof(int));
+    *ptr_c = 5;
+    val* c_val = int_val_constructor( ptr_c );
 
-    /* типу инт соответствует 0 */
-    aStruct->typeNum = 0;
-    cStruct->typeNum = 0;
-    /* типу char соответствует 1 */
-    bStruct->typeNum = 1;
+    val* d_val = nil_constructor();
 
 
-    val struct1 = *aStruct;
-    val struct2 = *bStruct;
-    val struct3 = *cStruct;
-
-    /* вызов построения списка */
-    cell * lst1 = makelist(3, struct1, struct2, struct3);
-
-    /* при отладке здесь все печатается правильно */
-    printf("main: lst1 %p\n", lst1);
-    printf("main: testPrintList lst1 from main: \n");
-    testPrintList(lst1);
-    testPrintList(lst1);
-    testPrintList(lst1);
+    /* распечатаем */
+    pprint( a_val );
+    printf( "\n" );
+    pprint( b_val );
+    printf( "\n" );
+    pprint( c_val );
+    printf( "\n" );
+    pprint( d_val );
+    printf( "\n" );
 
 
-    /* вызов построения нового списка */
-    cell * lst2 = testMakeList();
+    /* создадим список из одного эл-та = точечную пару (4 . NIL) */
+    val* single = cons( a_val, d_val );
 
-    /* при отладке здесь все печатается НЕ правильно */
-    printf("main: testPrintList lst2 from main: \n");
-    testPrintList(lst2);
+    /* распечатаем  */
+    ipprint( single );
+    printf( "\n" );
 
-    testPrintList(lst2);
 
-    testPrintList(lst2);
+    /* создадим точечную пару (4 . 5) */
+    val* dotpair = cons( a_val, c_val );
+
+    /* распечатаем её */
+    ipprint( dotpair );
+    printf( "\n" );
+
+
+    /* свяжем её с пятеркой спереди (5 4 . 5) */
+    val* foo = cons( c_val, dotpair );
+
+    /* распечатаем */
+    ipprint( foo );
+    printf( "\n" );
+
+    /* теперь соберем список (4 5) */
+    val* bar = cons( a_val, cons( c_val, d_val ));
+
+    /* распечатаем */
+    ipprint( bar );
+    printf( "\n" );
+
+    /* теперь соберем список (4 b 5) */
+    val* foobar = cons( a_val, cons ( b_val, cons( c_val, d_val )));
+
+    /* распечатаем */
+    ipprint( foobar );
+    printf( "\n" );
+
+    /* соберем вложенный список (4 (4 5) 5) */
+    val* baz = cons( a_val, cons ( cons( a_val,
+                                            cons( c_val, d_val )),
+                                      cons( c_val, d_val )));
+
+    /* распечатаем */
+    ipprint( baz );
+    printf( "\n" );
+
+    /* соберем вложенный список (4 ((4 5))) */
+    val* bazo = cons( a_val, cons (cons ( cons ( a_val,
+                                           cons( c_val, d_val )),
+                                          d_val),
+                                   d_val));
+    /* распечатаем */
+    ipprint( bazo );
+    printf( "\n" );
+
+    /* соберем список из одного элемента NIL - тут надо поправить */
+    val* bazon = cons( d_val, d_val );
+    /* распечатаем */
+    ipprint( bazon );
+    printf( "\n" );
+
 
     return 0;
 }
