86d0f3fe1e82a35843f71c727428bb8b072b4a12
0xBECEDA <sakharovasonya1@gmail.com>
2020-10-01 00:30:21 +0300

    some tests

--- exercise_51/read_input_test.sh
+++ exercise_51/read_input_test.sh
@@ -4,7 +4,41 @@ PROGA=proga
 
 if test -x "$PROGA"; then
 
-    ./proga | echo 'someshit'
+
+    echo "\"someshit"\" | ./proga
+
+    sleep 1
+
+    echo "\"(3)"\" | ./proga
+
+    sleep 1
+
+    echo "( 1 2 3)" | ./proga
+
+    sleep 1
+
+    echo "( 1 2 (3 4 ))" | ./proga
+
+    sleep 1
+
+    echo "( 1 2 '(3 4 ))" | ./proga
+
+    sleep 1
+
+    echo "34141" | ./proga
+
+    sleep 1
+
+    echo "\" Monday Friday Sommer Nacht "\" | ./proga
+
+    sleep 1
+
+    echo " \" 'Monday Friday Sommer Nacht \" () " | ./proga
+
+    sleep 1
+    echo "( \"( '(dsac) \" 1 2 'd + \"\" x)" | ./proga
+
+
 
 else
     echo "файл $PROGA не существует\n"
